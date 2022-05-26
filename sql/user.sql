d060ba3a574dad498839529a763bb138649bd218
hwimin-kim <gnlals7841@gmail.com>
2022-05-26 12:21:53 +0900

    sql update commit

--- sql/user.sql
+++ sql/user.sql
@@ -2,9 +2,17 @@
 
 desc user;
 
--- insert
+-- select
+select * from user;
+
+-- join
 insert
  into user
  values(null, '관리자', 'admin@mysite.com', '1234', 'male', now());
--- select
-select * from user;
+ 
+
+-- login
+select no, name
+ from user
+ where email = 'dooly@gmail.com'
+ and password = '1234';
