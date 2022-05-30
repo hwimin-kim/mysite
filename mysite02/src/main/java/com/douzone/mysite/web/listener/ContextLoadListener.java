9be1096710c3a18f71cd91e047a4e9a1c878df21
hwimin-kim <gnlals7841@gmail.com>
2022-05-30 11:21:15 +0900

    listener & filter

---
+++ mysite02/src/main/java/com/douzone/mysite/web/listener/ContextLoadListener.java
@@ -0,0 +1,15 @@
+package com.douzone.mysite.web.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class ContextLoadListener implements ServletContextListener {
+
+	public void contextInitialized(ServletContextEvent sce)  { 
+		System.out.println("Application starts....");
+	}
+    public void contextDestroyed(ServletContextEvent sce)  { 
+        
+    }
+	
+}
