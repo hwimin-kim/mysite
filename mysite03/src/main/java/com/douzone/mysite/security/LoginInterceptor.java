e54a534b74edea22abd50dca320630f9ea6961dd
hwimin-kim <gnlals7841@gmail.com>
2022-06-13 11:26:32 +0900

    LoginInterceptor #1

--- mysite03/src/main/java/com/douzone/mysite/security/LoginInterceptor.java
+++ mysite03/src/main/java/com/douzone/mysite/security/LoginInterceptor.java
@@ -25,7 +25,7 @@ public class LoginInterceptor implements HandlerInterceptor {
 			request.setAttribute("email", email);
 			request.setAttribute("password", "fail");
 			
-			request.getRequestDispatcher("/WEB-INF/views/user/login.jsp");
+			request.getRequestDispatcher("/WEB-INF/views/user/login.jsp").forward(request, response);
 			return false;
 		}
 			
